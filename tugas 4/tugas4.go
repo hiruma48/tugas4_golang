c7881dfbccd97b03c437b62a2056212ff5ae4f95
hiruma48 <49358000+hiruma48@users.noreply.github.com>
2023-11-12 18:36:10 +0700

    tugas 4 golang
    done

---
+++ tugas 4/tugas4.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nama_mahasiswa = map[string]int{"aldo": 182, "yosep": 178}
+	fmt.Println("aldo : ", nama_mahasiswa["aldo"], "cm")
+	fmt.Println("yosep : ", nama_mahasiswa["yosep"], "cm")
+
+}
